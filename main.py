22eb02c937d1caba19a5a665a79b1f943cf822ff
mehran <mehran.taheri.t@gmail.com>
2022-04-06 17:49:56 +0430

    save work

--- main.py
+++ main.py
@@ -25,3 +25,65 @@ def isBoardFull(board):
         return False
     else:
         return True
+
+def IsWinner(b,l):
+    return (b[1] == l and b[2] == l and b[3] == l) or \
+           (b[4] == l and b[5] == l and b[6] == l) or \
+           (b[7] == l and b[8] == l and b[9] == l) or \
+           (b[1] == l and b[4] == l and b[7] == l) or \
+           (b[2] == l and b[5] == l and b[8] == l) or \
+           (b[3] == l and b[6] == l and b[9] == l) or \
+           (b[1] == l and b[5] == l and b[9] == l) or \
+           (b[3] == l and b[5] == l and b[7] == l)
+
+def playerMove():
+    run = True
+    while run:
+        move = input ("please insert a position to enter the x between 1 to 9")
+        try:
+            move = int(move)
+            if move > 0  and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter("X", move)
+                else:
+                    print ("sorry,this space is occupied!")
+            else:
+                print ("please type a number between 0 and 9")
+        except:
+                print ("please enter a number")
+
+def computerMove():
+    possibleMoves = [x for x,lettet in enumerate(board) if letter == '' and x!= 0]
+    move = 0
+
+    for let in ['O','X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if IsWinner(boardCopy,let):
+                move = i
+                return move
+
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
